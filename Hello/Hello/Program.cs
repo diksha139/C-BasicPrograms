3e0e52d23433d61fa0ec0c80732b27eea7f6d92a
diksha139 <dikshabhupi139@gmail.com>
2024-08-23 14:43:21 +0530

    Add files via upload

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //just checking the initial code in c#
+
+            //vraiable declaration
+            int num1 = 100;
+           /* Console.WriteLine("Hello Diksha Welcome...");
+            Console.WriteLine("Hello Bhupi Welcome...");
+            Console.Write("how are you");
+            Console.Write(" Please say...");
+            Console.WriteLine(" You are " + num1 + " % Correct!!");
+
+            */
+
+
+           string input1 =  Console.ReadLine();
+            Console.WriteLine("your typed input value is : " + input1);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Perfect Square/Perfect Square/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perfect_Square
+{
+    internal class Program
+    {
+        public bool is_perfect_square(int num)
+        {
+            double x = Math.Sqrt(num);
+
+            if(x*x == num)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to check if its perfect square or not");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Program obj = new Program();
+            bool result = obj.is_perfect_square(number1);
+            if (result)
+            {
+                Console.WriteLine(number1+" is a perfect square");
+            }
+            else
+            {
+                Console.WriteLine("ops!! "+number1 + " is not a perfect square");
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
